5c30374e6fc30fbd054fc6b8484d5d0f0da710bf
Minh Le <lqm231@gmail.com>
2016-03-16 23:12:09 +0700

    Create website

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+    "use strict";
+    grunt.initConfig({
+	    src: {
+			app: "app",
+			lib: "lib",
+			less: "app/less",
+			css: "content/css"
+		},
+		usebanner: {
+		    dist: {
+				options: {
+					position: "replace",
+					banner: "/* lqminh */",
+					linebreak: true,
+					replace: true
+				},
+				files: {
+					src: [ "index.js" ]
+				}
+		    }
+		},
+		less: {
+            styles: {
+                options: {
+                    paths: [ "<%= src.less %>" ],
+                    compress: false
+                },
+                files: {
+                    "<%= src.css %>/main.css": "<%= src.less %>/main.less"
+                }
+            }
+        }
+	});
+
+	grunt.loadNpmTasks("grunt-banner");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-less");
+
+	grunt.registerTask("addbanner", [ "usebanner" ]);
+	grunt.registerTask("css", [ "less" ]);
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.set("port", (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + "/app"));
+app.use("/lib", express.static(__dirname + "/lib"));
+
+// views is directory for all template files
+// app.set("views", __dirname + "/views");
+// app.set("view engine", "ejs");
+
+app.get('/', function(req, res) {
+    // response.send("HELLO");
+    // response.render("index");
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
