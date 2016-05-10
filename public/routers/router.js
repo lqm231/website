33843b688709387304f6366461a53148c14d5d93
Minh Le <lqm231@gmail.com>
2016-05-11 00:39:38 +0700

    First Article

--- public/routers/router.js
+++ public/routers/router.js
@@ -1,7 +1,8 @@
 define([],
 function() {
     var defaultRouters = [
-        { route: "", moduleId: "viewmodels/home", nav: true }
+        { route: "", moduleId: "viewmodels/home", nav: true },
+        { route: "window", moduleId: "viewmodels/window", nav: true }
     ];
     return {
         defaultRouters: defaultRouters
